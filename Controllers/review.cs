d858f3f4f1e1212fdd3da74e16dca29284797f6e
Alka Santosh Naik <alkasantoshnaik@usf.edu>
2024-04-21 15:29:41 -0400

    Alka Santosh Naik Adding review page for crud operations

---
+++ Controllers/review.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DisProject.Controllers
+{
+	public class review : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
+
+
