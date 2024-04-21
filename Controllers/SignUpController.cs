3dc72017816aee2c6a5a1d8749965e9c9c1a9004
Venkata Krishna Sri Chandana Manuri <manuriv@usf.edu>
2024-04-21 14:37:46 -0400

    aaded book insights, and added gloabla varaible for  sign up so when signed up, user can store the information

--- Controllers/SignUpController.cs
+++ Controllers/SignUpController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using DisProject.Models;
+using DisProject.Global_Variables;
+
+using System.Collections.Generic;
 
 namespace DisProject.Controllers
 {
+    using DisProject.Global_Variables;
+
+    // SignUpController.cs
+    using Microsoft.AspNetCore.Mvc;
+
     public class SignUpController : Controller
     {
+        // Action to display the sign-up form
         public IActionResult Index()
         {
             return View();
         }
+
+        // Action to handle sign-up form submission
+        [HttpPost]
+        public IActionResult Create(SignUpModel model) // Here, SignUpModel model receives form data
+        {
+            if (ModelState.IsValid)
+            {
+                // Generate a unique ID for the user
+                int userId = GlobalData.Users.Count + 1;
+                model.Id = userId;
+
+                // Add the user to the global data
+                GlobalData.Users.Add(userId, model);
+
+                // Redirect to home page or show success message
+                return RedirectToAction("Index", "Success");
+            }
+            return View(model);
+        }
     }
+
+
 }
---
+++ Models/SignUpModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace DisProject.Models
+{
+
+public class SignUpModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
