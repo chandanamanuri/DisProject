40083209cd4506a3d9b7541723c763bbf4a53fff
Alka Santosh Naik <alkasantoshnaik@usf.edu>
2024-04-21 21:19:43 -0400

    Edits for the About page

---
+++ Controllers/SuccessReg.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DisProject.Controllers
+{
+    public class SuccessReg : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
